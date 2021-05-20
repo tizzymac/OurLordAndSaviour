40d3b75704c894700760b2cf57e900702d662531
Tizzy <tizzymac@icloud.com>
2021-05-20 15:33:48 -0600

    makes adder lib optional

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
-#include <adder.hpp>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <OLASConfig.h>
 
+#ifdef USE_ADDER
+    #include <adder.hpp>
+#endif
+
 int main(int argc, char *argv[])
 {
     std::cout << "Rejoice!" << std::endl;
-    std::cout << add(2, 2) << std::endl;
+
+#ifdef USE_ADDER
+    std::cout << "Using Adder lib: " << add(2, 2) << std::endl;
+#else
+    std::cout << 2 + 2 << std::endl;
+#endif
+
     std::cout << argv[0] << " Version: " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << std::endl;
 
     GLFWwindow *window;
